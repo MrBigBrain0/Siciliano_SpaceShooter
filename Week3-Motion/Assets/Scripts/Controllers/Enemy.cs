0b144eb31cdd08c465dc9da0ea0fbf296e80367a
MrBigBrain0 <sicilisi@sheridancollege.ca>
2024-09-24 00:52:01 -0400

    Task 2 is completed
    Task 2 is working, the enemy moves along the x-axis using Mathf ping pong, a more in depth is in the journal.

--- Week3-Motion/Assets/Scripts/Controllers/Enemy.cs
+++ Week3-Motion/Assets/Scripts/Controllers/Enemy.cs
@@ -3,8 +3,14 @@ using System.Collections;
 
 public class Enemy : MonoBehaviour
 {
+
+    public float speed = 3f;
+    public GameObject enemyPreFab;
     private void Update()
     {
+
+        transform.position = new Vector3(Mathf.PingPong(Time.time * speed, 15), transform.position.y, -20 );
+
     }
 
 }
