1b4484f1bf3592017aa1d170cd7cfbe9eba2ff14
MrBigBrain0 <sicilisi@sheridancollege.ca>
2024-10-14 17:08:54 -0400

    Final Assignment Task 2: Death Star Moon Start
    I started the Task 2 by just deleting some objects that aren't necessary for the task. deleted the planet from game space and the current code inside the moon script.

--- Week3-Motion/Assets/Scripts/Controllers/Moon.cs
+++ Week3-Motion/Assets/Scripts/Controllers/Moon.cs
@@ -5,24 +5,12 @@ using UnityEngine;
 
 public class Moon : MonoBehaviour
 {
-    public Transform planetTransform;
-    public Transform target;
 
-    public float raduis = 2f;
-    public float speed = 2f;
-
-    // Update is called once per frame
     void Update()
     {
-        OrbitalMotion(raduis, speed, target);
+        
     }
 
-    public void OrbitalMotion(float raduis, float speed, Transform target)
-    {
-        float degre = 360f;
-        float radians = degre * Mathf.Deg2Rad;
 
-
-    }
 
 }
